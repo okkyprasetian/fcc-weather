61a5b4fefa99cb21c3e76a666357e4f2cc49314a
okkyprasetian <okkyprasetian@gmail.com>
2022-02-21 09:28:57 +0700

    problem to display fetched data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import { useState, useEffect } from 'react'
+import Weather from './Weather'
 
 function App() {
 
   // State
   const [lat, setLat] = useState([])
   const [long, setLong] = useState([])
+  const [api] = useState('50e498c867d3e585514457c5f980432e')
+  const [data, setData] = useState([])
 
+  // Use Effect
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        console.log(position.coords.latitude)
-        console.log(position.coords.longitude)
-        setLat(position.coords.latitude)
-        setLong(position.coords.longitude)
-      })
-    } else {
-      console.log("Navigation Not Supported by This Browser")
-    }
+    const fetchData = async () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          setLat(position.coords.latitude)
+          setLong(position.coords.longitude)
+        })
+      } else {
+        console.log("Navigation Not Supported by This Browser")
+      }
 
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
-  }, [lat, long])
+      await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${api}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+        })
+    }
+    fetchData()
+  }, [lat, long, api])
+
 
   return (
     <div className="App">
+      {(typeof data == 'und') ? (
+        <Weather data={data} />
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
 }
---
+++ src/Weather.jsx
@@ -0,0 +1,13 @@
+function Weather({ data }) {
+    return (
+        <div className="card">
+            <h2 className="name">City Name: {data.timezone}</h2>
+            <p>Temperature: {data.current.temp}</p>
+            <p>Sunrise: {data.current.sunrise}</p>
+            <p>Sunset: {data.current.sunset}</p>
+            <p>Description: {data.weather[0].main}</p>
+        </div>
+    );
+}
+
+export default Weather;
