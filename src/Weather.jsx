0fa049218d4c9adc785a04baaf900df418bb4bfa
okkyprasetian <okkyprasetian@gmail.com>
2022-02-21 10:02:56 +0700

    finish convert all fetched data to common metric

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,11 +1,19 @@
 function Weather({ data }) {
+
+    const temp = (data.current.temp - 273.15).toFixed(2)
+    const sunrise = new Date(data.current.sunrise * 1000).toLocaleTimeString()
+    const sunset = new Date(data.current.sunset).toLocaleTimeString()
+    const icon = data.current.weather[0].icon
+    const weather = data.current.weather[0].description
+
     return (
         <div className="card">
-            <h2 className="name">City Name: {data.timezone}</h2>
-            <p>Temperature: {data.current.temp}</p>
-            <p>Sunrise: {data.current.sunrise}</p>
-            <p>Sunset: {data.current.sunset}</p>
-            <p>Description: {data.current.weather[0].main}</p>
+            <h2 className="name">Timezone: {data.timezone}</h2>
+            <p>Temperature: {temp} &deg;C </p>
+            <p>Sunrise: {sunrise}</p>
+            <p>Sunset: {sunset}</p>
+            <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="" />
+            <p>Weather: {weather}</p>
         </div>
     );
 }
