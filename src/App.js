da84219b1cc9c779e924d5d2cd87b10a56a3fc77
okkyprasetian <okkyprasetian@gmail.com>
2022-02-21 08:02:01 +0700

    create function to get latitude and longitude

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  // State
+  const [lat, setLat] = useState([])
+  const [long, setLong] = useState([])
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords.latitude)
+        console.log(position.coords.longitude)
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+      })
+    } else {
+      console.log("Navigation Not Supported by This Browser")
+    }
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long])
+
   return (
     <div className="App">
     </div>
